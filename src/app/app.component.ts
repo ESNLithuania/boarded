3d7bb9f6629f9a04668d570bb905fdab843cf7c5
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-07-17 21:01:22 +0300

    working tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { NavbarComponent } from './shared/index';
 import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES, ROUTER_DIRECTIVES, NavbarComponent]
+  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, ROUTER_DIRECTIVES, NavbarComponent]
 })
 export class AppComponent { }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router'
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs';
+
 @Component({
     selector: 'b-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css'],
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, MD_TABS_DIRECTIVES]
 })
 export class NavbarComponent {}
