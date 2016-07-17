91de1ee2cc083b47ab0366067e4f20a33f3ef95c
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-07-17 18:58:02 +0300

    included directives for navbar and added to index ts for better maintanability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
-
+import { NavbarComponent } from './shared/index';
 import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES, ROUTER_DIRECTIVES]
+  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES, ROUTER_DIRECTIVES, NavbarComponent]
 })
 export class AppComponent { }
