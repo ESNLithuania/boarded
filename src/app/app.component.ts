1126ee4eadf0f7dcab64a2ef1d004f1d27a0c8bc
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-07-17 12:24:14 +0300

    basic intro page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+
 import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES]
 })
 export class AppComponent { }
