770b097f2e2d6256cb412a6f50ed0026cbe162e6
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-08-27 19:01:46 +0300

    added footer

---
+++ src/app/modules/shared/footer.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+@Component({
+  selector: 'esn-footer',
+  templateUrl: 'footer.component.html',
+  styleUrls: ['footer.component.scss']
+})
+export class FooterComponent {}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from "@angular/core";
 import {NavigationComponent} from "./navigation.component";
 import {CommonModule} from "@angular/common";
+import {FooterComponent} from "./footer.component";
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [NavigationComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [NavigationComponent]
+  exports: [NavigationComponent, FooterComponent]
 })
 
 export default class SharedModule {}
