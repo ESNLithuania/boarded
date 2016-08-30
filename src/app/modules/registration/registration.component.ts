440b98ef1831fb07b670c0e8db876d4bb3c4860a
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-08-30 22:01:54 +0300

    added submitted state for form

--- src/app/modules/registration/registration.component.ts
+++ src/app/modules/registration/registration.component.ts
@@ -1,20 +1,24 @@
 import {Component} from "@angular/core";
 import {User} from "./user";
 import {RegistrationService} from "./registration.service";
-import {Observable} from "rxjs";
 
 @Component({
   selector: 'esn-registration',
   templateUrl: './registration.component.html'
 })
 export class RegistrationComponent {
-  constructor(private registrationService: RegistrationService) {}
-  user =  new User();
+  constructor(private registrationService: RegistrationService) {
+  }
+
+  user = new User();
+
+  submitted = false;
 
   onSubmit() {
-    console.log('submitted!');
     this.registrationService.addUser(this.user)
-      .subscribe((res: User) => console.log(res));
+      .subscribe((res: User) => {
+        this.submitted = true;
+      });
   }
 
   sections = [
