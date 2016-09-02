261700eeb179c5fd86c50e56d43e4989c46512f3
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-09-02 18:41:03 +0300

    added main user service methods

--- src/app/modules/services/user.service.ts
+++ src/app/modules/services/user.service.ts
@@ -1,3 +1,24 @@
 import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
 @Injectable
-export class UserService {}
+export class UserService {
+  private loggedIn = false;
+
+  constructor(private http: Http) {
+    this.loggedIn = !!localStorage.getItem('auth_token');
+  }
+
+  public login(email, password) {
+    localStorage.setItem('auth_token', 'loggedIn');
+    this.loggedIn = true;
+  }
+
+  public logout() {
+    localStorage.removeItem('auth_token');
+    this.loggedIn = false;
+  }
+
+  public isLoggedIn() {
+    return this.loggedIn;
+  }
+}
