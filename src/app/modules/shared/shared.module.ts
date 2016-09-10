39e032591876cac21045086520ac46e30d0a7a41
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-09-10 14:29:15 +0300

    router working with navigation

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,9 +3,10 @@ import {NavigationComponent} from "./navigation.component";
 import {CommonModule} from "@angular/common";
 import {FooterComponent} from "./footer.component";
 import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 @NgModule({
   declarations: [NavigationComponent, FooterComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   exports: [NavigationComponent, FooterComponent]
 })
 
