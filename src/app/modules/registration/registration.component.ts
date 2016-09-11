a18c5d5d88d73289c928c55c507035685d7ef6a8
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-09-11 11:38:25 +0300

    working tabs and registration

--- src/app/modules/registration/registration.component.ts
+++ src/app/modules/registration/registration.component.ts
@@ -12,11 +12,19 @@ export class RegistrationComponent {
   constructor(private registrationService: RegistrationService) {
   }
 
+  tabIds = ['basic-info', 'section-info', 'address-info'];
+  selectedId = this.tabIds[0];
+
+  public toggleSelected() {
+    this.selectedId = this.tabIds[(this.tabIds.indexOf(this.selectedId) + 1)];
+  }
+
   user: User = new User();
 
   submitted = false;
 
   onSubmit() {
+    console.log(this.user);
     this.registrationService.addUser(this.user)
       .subscribe((res: User) => {
         this.submitted = true;
