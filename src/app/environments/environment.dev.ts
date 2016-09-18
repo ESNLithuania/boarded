9744a9b3fbb575adbdb603b5f5d30cdc0d4afb6f
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-09-18 14:03:56 +0300

    added firebase to environment vars

--- src/app/environments/environment.dev.ts
+++ src/app/environments/environment.dev.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "<your-key>",
+    authDomain: "<your-project-authdomain>",
+    databaseURL: "<your-database-URL>",
+    storageBucket: "<your-storage-bucket>"
+  }
 };
